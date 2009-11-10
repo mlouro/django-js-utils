348425ba44935a8ac24a99e051d272e0dab20220
Marco Louro <mlouro@gmail.com>
2009-11-10 20:17:50 +0000

    fixes for jslint compliance

--- dutils.js
+++ dutils.js
@@ -6,24 +6,33 @@ dutils.urls = function(){
     function _get_path(name, kwargs, urls)
     {
 
-        var path = urls[name] || undefined;
+        var path = urls[name] || false;
 
         if (!path)
+        {
             throw('URL not found for view: ' + name);
+        }
 
         var _path = path;
 
+        var key;
         for (key in kwargs)
         {
-            if (!path.match('\<' + key +'\>'))
-                throw(key + ' does not exist in ' + _path);
-            path = path.replace('\<' + key +'\>', kwargs[key]);
+            if (kwargs.hasOwnProperty(key)) {
+                if (!path.match('<' + key +'>'))
+                {
+                    throw(key + ' does not exist in ' + _path);
+                }
+                path = path.replace('<' + key +'>', kwargs[key]);
+            }
         }
 
-        var re = new RegExp('\<[a-zA-Z0-9-_]{1,}\>', 'g');
+        var re = new RegExp('<[a-zA-Z0-9-_]{1,}>', 'g');
         var missing_args = path.match(re);
         if (missing_args)
+        {
             throw('Missing arguments (' + missing_args.join(", ") + ') for url ' + _path);
+        }
 
         return path;
 
@@ -33,11 +42,13 @@ dutils.urls = function(){
 
         resolve: function(name, kwargs, urls) {
             if (!urls)
+            {
                 urls = dutils.conf.urls || {};
+            }
 
             return _get_path(name, kwargs, urls);
         }
 
-    }
+    };
 
 }();
